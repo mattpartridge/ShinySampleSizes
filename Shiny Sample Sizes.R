e9a28f88aaacd197d7b171dff1df6d1f29ea516f
mattpartridge <mfpartridge92@gmail.com>
2016-01-30 14:24:02 -0600

    Interactive Outputs
    One proportion and two proportions are now fully interactive with
    numericInput()s.

--- Shiny Sample Sizes.R
+++ Shiny Sample Sizes.R
@@ -2,11 +2,6 @@
 # Advisor: Julian Wolfson
 # Shiny Sample Sizes
 
-####################
-### Things to Do ###
-####################
-# Update textOutput()s to numericInput()s and make the updatable
-
 
 #################
 ### Libraries ###
@@ -33,17 +28,17 @@ shinyApp(
                     numericInput(inputId = "Noneprop", label = "N", min = 0, max = NA, value = 100),
                     sliderInput(inputId = "pioneprop", label = "Pi", min = 0, max = 1, value = 0.5),
                     numericInput(inputId = "confoneprop", label = "Confidence", min = 0, max = 1, value = 0.95, step = 0.05),
-                    textOutput("MOEoneprop"),
-                    textOutput("CIoneprop"))
-                  # box(
-                    # sliderInput(inputId = "moeoneprop", label = "Margin of Error", min = 0, max = 80, value = 0.5)
-                  # )
-                )),
+                    numericInput(inputId = "MOEonepropa", label = "Margin of Error", value = 0.098),
+                    sliderInput(inputId = "CIonepropa", label = "Confidence Interval", min = 0, max = 1, value = c(0.402, 0.598))))),
         
         #############################################
         ############## Two Proportions ##############
         #############################################
         tabItem(tabName = "twoproportions",
+                fluidRow(
+                  column(12,
+                    selectInput(inputId = "SolveFortwoprops", label = "Solve For",
+                                choices = c("Significance", "Power", "N")))),
                 fluidRow(
                   box(
                     sliderInput(inputId = "alphatwoprops", label = "Significance:",
@@ -57,11 +52,8 @@ shinyApp(
                                 min = 0, max = 1, value = 0.4))),
                 fluidRow(
                   box(
-                    textOutput("n.per.group.props"),
-                    textOutput("TotalN.props"),
-                    textOutput("TEST"),
-                    selectInput(inputId = "SolveFortwoprops", label = "Solve For",
-                                choices = c("Significance", "Power"))))),
+                    numericInput(inputId = "n.per.group.twopropa", label = "Number of People per Group", value = 385),
+                    numericInput(inputId = "TotalN.twopropa", label = "Total Number of People", value = 770)))),
         
         #######################################
         ############## Two Means ##############
@@ -101,55 +93,6 @@ shinyApp(
   
   server = function(input, output, clientData, session){
     
-    ############################################
-    ############## One Proportion ##############
-    ############################################
-    output$MOEoneprop <- renderText({
-      pi = input$pioneprop
-      N = input$Noneprop
-      confidence = input$confoneprop
-      alpha = 1 - confidence
-      Z_alpha2 = qnorm(1 - (alpha/2))
-      se = sqrt((pi*(1 - pi))/N)
-      moe = round(Z_alpha2*se, digits = 3)
-      paste("The Margin of Error is: ", moe)})
-    output$CIoneprop = renderText({
-      pi = input$pioneprop
-      N = input$Noneprop
-      confidence = input$confoneprop
-      alpha = 1 - confidence
-      Z_alpha2 = qnorm(1 - (alpha/2))
-      se = sqrt((pi*(1 - pi))/N)
-      moe = Z_alpha2*se
-      lo = round(pi - moe, digits = 3)
-      hi = round(pi + moe, digits = 3)
-      paste("The confidence interval is: ", lo, ", ", hi)})
-    
-    #############################################
-    ############## Two Proportions ##############
-    #############################################
-    output$TEST = renderText({
-      ifelse(input$solvefor == "alpha", paste("alpha"), ifelse(input$solvefor == "Power", paste("Power"), paste("Sample Size")))})
-    output$n.per.group.props <- renderText({
-      alpha = (100 - input$alphatwoprops)/100
-      beta = input$betatwoprops/100
-      p1 = input$p1
-      p2 = input$p2
-      Z.alpha.2 = qnorm(1 - alpha/2)
-      Z.beta = qnorm(beta)
-      n = ceiling(((Z.alpha.2 + Z.beta)^2 * (p1*(1 - p1) + (p2*(1 - p2)))) / ((p1 - p2)^2))
-      paste("The number of people needed per group is: ", n)})
-    output$TotalN.props <- renderText({
-      alpha = (100 - input$alphatwoprops)/100
-      beta = input$betatwoprops/100
-      p1 = input$p1
-      p2 = input$p2
-      Z.alpha.2 = qnorm(1 - alpha/2)
-      Z.beta = qnorm(beta)
-      n = ceiling(((Z.alpha.2 + Z.beta)^2 * (p1*(1 - p1) + (p2*(1 - p2)))) / ((p1 - p2)^2))
-      N = 2*n
-      paste("The total number of people needed is: ", N)})
-    
     #######################################
     ############## Two Means ##############
     #######################################
@@ -183,21 +126,57 @@ shinyApp(
     })
     
     observe({
+      
+      ############################################
+      ############## One Proportion ##############
+      ############################################
+      # Margin of Error
+      pi = input$pioneprop
+      N = input$Noneprop
+      confidence = input$confoneprop
+      alpha = 1 - confidence
+      Z_alpha2 = qnorm(1 - (alpha/2))
+      se = sqrt((pi*(1 - pi))/N)
+      moe = round(Z_alpha2*se, digits = 3)
+      lo = round(pi - moe, digits = 3)
+      hi = round(pi + moe, digits = 3)
+      updateNumericInput(session, inputId = "MOEonepropa", value = moe)
+      updateSliderInput(session, inputId = "CIonepropa", value = c(lo, hi))
+    
+      #############################################
+      ############## Two Proportions ##############
+      #############################################
+      if(input$SolveFortwoprops == "Significance"){
+        SFalpha = round(2*(1 - pnorm( sqrt(input$n.per.group.twopropa*((input$p1 - input$p2)^2)/(input$p1*(1-input$p1) + input$p2*(1-input$p2))) - qnorm(input$betatwoprops/100) )), digits = 3)
+        SFSignificance = 100*(1 - SFalpha)
+        updateSliderInput(session, inputId = "alphatwoprops", value = SFSignificance)}
+      if(input$SolveFortwoprops == "Power"){
+        SFbeta = round((pnorm( sqrt(input$n.per.group.twopropa*((input$p1 - input$p2)^2)/(input$p1*(1-input$p1) + input$p2*(1-input$p2))) - qnorm(1 - ((100 - input$alphatwoprops)/100)/2) )), digits = 3)
+        SFPower = 100*(SFbeta)
+        updateSliderInput(session, inputId = "betatwoprops", value = SFPower)}
+      if(input$SolveFortwoprops == "N"){
+        alpha = (100 - input$alphatwoprops)/100
+        beta = input$betatwoprops/100
+        p1 = input$p1
+        p2 = input$p2
+        Z.alpha.2 = qnorm(1 - alpha/2)
+        Z.beta = qnorm(beta)
+        n = ceiling(((Z.alpha.2 + Z.beta)^2 * (p1*(1 - p1) + (p2*(1 - p2)))) / ((p1 - p2)^2))
+        N = 2*n
+        updateNumericInput(session, inputId = "n.per.group.twopropa", value = n)
+        updateNumericInput(session, inputId = "TotalN.twopropa", value = N)}
+      
+      ##################################
+      ############## Test ##############
+      ##################################
       if(input$SolveFor == "Control"){
         SFResponse = 0.5*input$Response
         updateSliderInput(session, inputId = "Control", value = SFResponse)}
       else{ updateSliderInput(session, inputId = "Response", value = 2*(input$Control)) }
       updateNumericInput(session, inputId = "controlresponse", value = (input$Control + input$Response))
-      
-#       if(input$SolveFortwoprops == "Significance"){
-#                 SFalpha = round(2*(1 - pnorm( sqrt(output$n.per.group.props*((input$p1 - input$p2)^2)/(input$p1*(1-input$p1) + input$p2*(1-input$p2))) - qnorm(input$betatwoprops/100) )), digits = 3)
-#                 SFSignificance = 100*(1 - SFalpha)
-#                 updateSliderInput(session, inputId = "alphatwoprops", value = SFSignificance)
-#         
-#       }
-#       else{}
       })
     
-  }
-)
+  } # Server Function
+) # Shiny App
+
 
