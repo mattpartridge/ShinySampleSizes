7a36746b0bb592e75e5f75440bbb09bbdd20dd17
mattpartridge <mfpartridge92@gmail.com>
2016-04-05 14:28:33 -0500

    Solve For
    I added back in the solve for because it makes sense for these tabs.

--- server.R
+++ server.R
@@ -5,15 +5,31 @@
 ###############################################
 ############## Things To Look At ##############
 ###############################################
-# Slider for N or add by increments
-# Adjusting ranges for slider as you drag --> Kind of implemented (Could look better)
-# Max n for slider would be n that gives 99% power --> Still need to implement
+# Number of "#" signifiy priority:
+##### Add solve for back --> Everything is connected to the sliders
+##### Add numeric boxes for everything --> Need to connect to sliders
+##### Add a final sentence compiling everything into one
+##### Max n for slider would be n that gives 99% power --> Still need to implement
+### Look into packages that do it for your; power.t.test/package(pwr)/package(pwerSurvEpi)
+# Committee --> (Nicole Basta?, Sonja Brady, people who actually design studies)?/Jim Neaton(Biostat)/Julian Wolfson(Biostat)
+# PASS
+# One sample means
+########## Tim to event (Potentially ask Jim what he uses)
+########## Read up on everything first
+########## nSurvival/nSurv/gsDesign (group sequential)
+########## r time to event
+########## GOAL: Have some thoughts about what the interface would look like (Even on paper)
+# Representative CI
+# Arrows()
+# Exact CI vs Wald CI for small p
+# Maybe something "Fancier"? --> Time to Event
+# Look for plots that could help understanding
 # Unequal groups
 
 
-library(shiny); library(shinydashboard)
+library(shiny); library(shinydashboard); library(pwr)
 server = function(input, output, clientData, session){
-
+  
   ############################################
   ############## One Proportion ##############
   ############################################
@@ -30,10 +46,10 @@ server = function(input, output, clientData, session){
     updateNumericInput(session, inputId = "MOEoneprop", value = moe)
     updateSliderInput(session, inputId = "CIoneprop", value = c(lo, hi))
   })
-
-    #######################################
-    ############## Two Means ##############
-    #######################################
+  
+  #######################################
+  ############## Two Means ##############
+  #######################################
   observe({
     alpha = input$alphatwomeans
     beta = input$betatwomeans/100
@@ -50,58 +66,39 @@ server = function(input, output, clientData, session){
   #############################################
   ############## Two Proportions ##############
   #############################################
-  Last_Widget_2prop = reactiveValues( last = NULL )
-  observe({ input$alphatwoprops; Last_Widget_2prop$last = "alpha"})
-  observe({ input$betatwoprops; Last_Widget_2prop$last = "beta" })
-  observe({ input$p1; Last_Widget_2prop$last = "p1" })
-  observe({ input$p2; Last_Widget_2prop$last = "p2" })
-  observe({ input$n.per.group.twoprops; Last_Widget_2prop$last = "n" })
-  observe({ input$TotalN.twoprops; Last_Widget_2prop$last = "N" })
+#   Last_Widget_2prop = reactiveValues( last = NULL )
+#   observe({ input$alphatwoprops; Last_Widget_2prop$last = "alpha"})
+#   observe({ input$betatwoprops; Last_Widget_2prop$last = "beta" })
+#   observe({ input$p1; Last_Widget_2prop$last = "p1" })
+#   observe({ input$p2; Last_Widget_2prop$last = "p2" })
+#   observe({ input$n.per.group.twoprops; Last_Widget_2prop$last = "n" })
+#   observe({ input$TotalN.twoprops; Last_Widget_2prop$last = "N" })
   observe({
-    if(Last_Widget_2prop$last %in% c("alpha", "p1", "p2")){
-      alpha = input$alphatwoprops
-      beta = input$betatwoprops/100
-      p1 = input$p1
-      p2 = input$p2
+    if(input$solvefortwoprops == "Sample Size"){
+      alpha = input$alphatwoprops.s
+      beta = input$betatwoprops.s/100
+      p1 = input$p1.s
+      p2 = input$p2.s
       Z.alpha.2 = qnorm(1 - alpha/2)
       Z.beta = qnorm(beta)
       n = ceiling(((Z.alpha.2 + Z.beta)^2 * (p1*(1 - p1) + (p2*(1 - p2)))) / ((p1 - p2)^2))
       N = 2*n
-      updateSliderInput(session, inputId = "n.per.group.twoprops", min = 0, max = n + n/2, value = n)
-      updateSliderInput(session, inputId = "TotalN.twoprops", min = 0, max = N + n, value = N)}
-    if(Last_Widget_2prop$last == "beta"){
-      alpha = input$alphatwoprops
-      beta = input$betatwoprops/100
-      p1 = input$p1
-      p2 = input$p2
-      Z.alpha.2 = qnorm(1 - alpha/2)
-      Z.beta = qnorm(beta)
-      n = ceiling(((Z.alpha.2 + Z.beta)^2 * (p1*(1 - p1) + (p2*(1 - p2)))) / ((p1 - p2)^2))
-      N = 2*n
-      updateSliderInput(session, inputId = "n.per.group.twoprops", min = 0, max = n + n/2, value = n)
-      updateSliderInput(session, inputId = "TotalN.twoprops", min = 0, max = N + n, value = N)}
-    if(Last_Widget_2prop$last == "n"){
-      SFbeta = round(pnorm( sqrt(input$n.per.group.twoprops*((input$p1 - input$p2)^2)/(input$p1*(1-input$p1) + input$p2*(1-input$p2))) - qnorm(1-input$alphatwoprops/2) ), digits = 1)
+      updateNumericInput(session, inputId = "n.per.group.twoprops.n", min = 0, max = n + n/2, value = n)
+      updateSliderInput(session, inputId = "n.per.group.twoprops.s", min = 0, max = n + n/2, value = n)
+      updateNumericInput(session, inputId = "TotalN.twoprops.n", min = 0, max = N + n, value = N)
+      updateSliderInput(session, inputId = "TotalN.twoprops.s", min = 0, max = N + n, value = N)
+    }
+    if(input$solvefortwoprops == "Power"){
+      n = input$n.per.group.twoprops.s
+      p1 = input$p1.s
+      p2 = input$p2.s
+      alpha = input$alphatwoprops.s
+      SFbeta = round(pnorm( sqrt(n*((p1 - p2)^2)/(p1*(1-p1) + p2*(1-p2))) - qnorm(1-alpha/2) ), digits = 1)
       SFPower = 100*(SFbeta)
-      updateSliderInput(session, inputId = "betatwoprops", value = SFPower)
-      updateSliderInput(session, inputId = "TotalN.twoprops", min = 0, max = 3*input$n.per.group.twoprops, value = 2*input$n.per.group.twoprops)}
-    if(Last_Widget_2prop$last == "N"){
-      SFbeta = round(pnorm( sqrt((0.5*input$TotalN.twoprops)*((input$p1 - input$p2)^2)/(input$p1*(1-input$p1) + input$p2*(1-input$p2))) - qnorm(1-input$alphatwoprops/2) ), digits = 1)
-      SFPower = 100*(SFbeta)
-      updateSliderInput(session, inputId = "betatwoprops", value = SFPower)
-      updateSliderInput(session, inputId = "n.per.group.twoprops", min = 0, max = input$TotalN.twoprops, value = 0.5*input$TotalN.twoprops)}
+      updateNumericInput(session, inputId = "betatwoprops.n", value = SFPower)
+      updateSliderInput(session, inputId = "betatwoprops.s", value = SFPower)
+#       updateNumericInput(session, inputId = "TotalN.twoprops.n", min = 0, max = 3*input$n.per.group.twoprops, value = 2*input$n.per.group.twoprops)
+#       updateSliderInput(session, inputId = "TotalN.twoprops.s", min = 0, max = 3*input$n.per.group.twoprops, value = 2*input$n.per.group.twoprops)
+      }
   })
-
-  ##################################
-  ############## Test ##############
-  ##################################
-  last_updated_widget <- reactiveValues( last = 0 )
-  observe({ input$Test1; last_updated_widget$last = 1 })
-  observe({ input$Test2; last_updated_widget$last = 2 })
-  observe({
-    if(last_updated_widget$last %in% c(0, 1)){ updateSliderInput(session, inputId = "Test2", value = .5*input$Test1) }
-    if(last_updated_widget$last %in% c(0, 2)){ updateSliderInput(session, inputId = "Test1", value = 2*input$Test2) }
-  })
-  output$LASTUPDATED = renderText(last_updated_widget$last)
-  
 } # Server Function
--- ui.R
+++ ui.R
@@ -5,21 +5,16 @@
 ###############################################
 ############## Things To Look At ##############
 ###############################################
-# Slider for N or add by increments
-# Adjusting ranges for slider as you drag --> Kind of implemented (Could look better)
-# Max n for slider would be n that gives 99% power --> Still need to implement
-# Unequal groups
+# See server.R
 
-
-library(shiny); library(shinydashboard)
+library(shiny); library(shinydashboard); library(pwr)
 ui = dashboardPage(
   dashboardHeader(title = "Sample Size Calculations"),
   dashboardSidebar(
     sidebarMenu(
       menuItem("One Sample Proportion", tabName = "oneproportion"),
       menuItem("Two Sample Proportions", tabName = "twoproportions"),
-      menuItem("Two Means", tabName = "twomeans"),
-      menuItem("reactiveValues() Test", tabName = "Test"))),
+      menuItem("Two Means", tabName = "twomeans"))),
   dashboardBody(
     tabItems(
       
@@ -40,49 +35,84 @@ ui = dashboardPage(
       #############################################
       tabItem(tabName = "twoproportions",
               fluidRow(
-                box(
-                  numericInput(inputId = "alphatwoprops", label = "Alpha Level:",
-                               min = 0, max = 1, value = 0.05, step = 0.01),
-                  sliderInput(inputId = "betatwoprops", label = "Power:",
-                              min = 0, max = 100, value = 80.0),
-                  uiOutput(outputId = "PowerTwoProps")),
-                box(
-                  sliderInput(inputId = "p1", label = "Proportion Affected by Treatment 1:",
-                              min = 0, max = 1, value = 0.5),
-                  sliderInput(inputId = "p2", label = "Proportion Affected by Treatment 2:",
-                              min = 0, max = 1, value = 0.4))),
+                box(width = 12,
+                    selectInput(inputId = "solvefortwoprops", label = "Solve For", choices = c("Sample Size", "Power"), width = "100%"))),
               fluidRow(
-                box(
-                  sliderInput(inputId = "n.per.group.twoprops", label = "Number of People per Group", min = 0, max = 10000, value = 385),
-                  sliderInput(inputId = "TotalN.twoprops", label = "Total Number of People", min = 0, max = 10000, value = 770, step = 2)))),
-      
-      #######################################
-      ############## Two Means ##############
-      #######################################
-      tabItem(tabName = "twomeans",
-              fluidRow(
-                box(
-                  numericInput(inputId = "alphatwomeans", label = "Alpha Level:",
-                               min = 0, max = 1, value = 0.05),
-                  sliderInput(inputId = "betatwomeans", label = "Power:",
-                              min = 0, max = 100, value = 80.0)),
-                box(
-                  numericInput(inputId = "deltamu", label = "Difference In Means", value = ""),
-                  numericInput(inputId = "sd", label = "Standard Deviation", value = ""))),
-              fluidRow(
-                box(
-                  numericInput(inputId = "n.per.group.twomeans", label = "Number of People per Group", value = ""),
-                  numericInput(inputId = "TotalN.twomeans", label = "Total Number of People", value = "")))),
-      
-      ##################################
-      ############## Test ##############
-      ##################################
-      tabItem(tabName = "Test",
-              fluidRow(
-                box(
-                  sliderInput(inputId = "Test1", label = "Test1", min = 0, max = 50, value = 50),
-                  sliderInput(inputId = "Test2", label = "Test2", min = 0, max = 50, value = 25),
-                  textOutput("LASTUPDATED"))))
-    ) # tabItems
-  ) # dashboardBody
-) # dashboardPage
+                # Inputs
+                box(width = 6,
+                    # Alpha
+                    numericInput(inputId = "alphatwoprops.n", label = "Alpha Level:", min = 0, max = 1, value = 0.05, step = 0.01),
+                    sliderInput(inputId = "alphatwoprops.s", label = NULL, min = 0, max = 1, value = 0.05, step = 0.01),
+                    # Beta
+                    numericInput(inputId = "betatwoprops.n", label = "Power:", min = 0, max = 100, value = 80.0),
+                    sliderInput(inputId = "betatwoprops.s", label = NULL, min = 0, max = 100, value = 80.0),
+                    # Proportions
+                    numericInput(inputId = "p1.n", label = "Proportion Affected by Treatment 1:", min = 0, max = 1, value = 0.5, step = 0.01),
+                    sliderInput(inputId = "p1.s", label = NULL, min = 0, max = 1, value = 0.5, step = 0.01),
+                    numericInput(inputId = "p2.n", label = "Proportion Affected by Treatment 2:", min = 0, max = 1, value = 0.4, step = 0.01),
+                    sliderInput(inputId = "p2.s", label = NULL, min = 0, max = 1, value = 0.4, step = 0.01)),
+                # Sample Size
+                #                   conditionalPanel("input.solvefortwoprops == 'Sample Size'",
+                #                                    # Alpha
+                #                                    numericInput(inputId = "alphatwoprops.n", label = "Alpha Level:", min = 0, max = 1, value = 0.05, step = 0.01),
+                #                                    sliderInput(inputId = "alphatwoprops.s", label = NULL, min = 0, max = 1, value = 0.05, step = 0.01),
+                #                                    # Beta
+                #                                    numericInput(inputId = "betatwoprops.n", label = "Power:", min = 0, max = 100, value = 80.0),
+                #                                    sliderInput(inputId = "betatwoprops.s", label = NULL, min = 0, max = 100, value = 80.0),
+                #                                    # Proportions
+                #                                    numericInput(inputId = "p1.n", label = "Proportion Affected by Treatment 1:", min = 0, max = 1, value = 0.5, step = 0.01),
+                #                                    sliderInput(inputId = "p1.s", label = NULL, min = 0, max = 1, value = 0.5, step = 0.01),
+                #                                    numericInput(inputId = "p2.n", label = "Proportion Affected by Treatment 2:", min = 0, max = 1, value = 0.4, step = 0.01),
+                #                                    sliderInput(inputId = "p2.s", label = NULL, min = 0, max = 1, value = 0.4, step = 0.01)),
+                #                 # Power
+                #                 conditionalPanel("input.solvefortwoprops == 'Power'",
+                #                                  # Alpha
+                #                                  numericInput(inputId = "alphatwoprops.n", label = "Alpha Level:", min = 0, max = 1, value = 0.05, step = 0.01),
+                #                                  sliderInput(inputId = "alphatwoprops.s", label = NULL, min = 0, max = 1, value = 0.05, step = 0.01),
+                #                                  # Group Sizes
+                #                                  numericInput(inputId = "n.per.group.twoprops.n", label = "Number of People per Group", min = 0, max = 10000, value = 385),
+                #                                  sliderInput(inputId = "n.per.group.twoprops.s", label = NULL, min = 0, max = 10000, value = 385),
+                #                                  numericInput(inputId = "TotalN.twoprops.n", label = "Total Number of People", min = 0, max = 10000, value = 770, step = 2),
+                #                                  sliderInput(inputId = "TotalN.twoprops.s", label = NULL, min = 0, max = 10000, value = 770, step = 2),
+                #                                  # Proportions
+                #                                  numericInput(inputId = "p1.n", label = "Proportion Affected by Treatment 1:", min = 0, max = 1, value = 0.5, step = 0.01),
+                #                                  sliderInput(inputId = "p1.s", label = NULL, min = 0, max = 1, value = 0.5, step = 0.01),
+                #                                  numericInput(inputId = "p2.n", label = "Proportion Affected by Treatment 2:", min = 0, max = 1, value = 0.4, step = 0.01),
+                #                                  sliderInput(inputId = "p2.s", label = NULL, min = 0, max = 1, value = 0.4, step = 0.01))),
+                # Outputs
+                box(width = 6,
+                    numericInput(inputId = "n.per.group.twoprops.n", label = "Number of People per Group", min = 0, max = 10000, value = 385),
+                    sliderInput(inputId = "n.per.group.twoprops.s", label = NULL, min = 0, max = 10000, value = 385),
+                    numericInput(inputId = "TotalN.twoprops.n", label = "Total Number of People", min = 0, max = 10000, value = 770, step = 2),
+                    sliderInput(inputId = "TotalN.twoprops.s", label = NULL, min = 0, max = 10000, value = 770, step = 2)))),
+                # Sample Size
+                #                 conditionalPanel("input.solvefortwoprops == 'Sample Size'",
+                #                                  # Group Sizes
+                #                                  numericInput(inputId = "n.per.group.twoprops.n", label = "Number of People per Group", min = 0, max = 10000, value = 385),
+                #                                  sliderInput(inputId = "n.per.group.twoprops.s", label = NULL, min = 0, max = 10000, value = 385),
+                #                                  numericInput(inputId = "TotalN.twoprops.n", label = "Total Number of People", min = 0, max = 10000, value = 770, step = 2),
+                #                                  sliderInput(inputId = "TotalN.twoprops.s", label = NULL, min = 0, max = 10000, value = 770, step = 2)),
+                #                 # Power
+                #                 conditionalPanel("input.solvefortwoprops == 'Power'",
+                #                                  # Beta
+                #                                  numericInput(inputId = "betatwoprops.n", label = "Power:", min = 0, max = 100, value = 80.0),
+                #                                  sliderInput(inputId = "betatwoprops.s", label = NULL, min = 0, max = 100, value = 80.0))))),
+                
+                #######################################
+                ############## Two Means ##############
+                #######################################
+                tabItem(tabName = "twomeans",
+                        fluidRow(
+                          box(
+                            numericInput(inputId = "alphatwomeans", label = "Alpha Level:", min = 0, max = 1, value = 0.05),
+                            sliderInput(inputId = "betatwomeans", label = "Power:", min = 0, max = 100, value = 80.0)),
+                          box(
+                            numericInput(inputId = "deltamu", label = "Difference In Means", value = ""),
+                            numericInput(inputId = "sd", label = "Standard Deviation", value = ""))),
+                        fluidRow(
+                          box(
+                            numericInput(inputId = "n.per.group.twomeans", label = "Number of People per Group", value = ""),
+                            numericInput(inputId = "TotalN.twomeans", label = "Total Number of People", value = ""))))
+              ) # tabItems
+      ) # dashboardBody
+    ) # dashboardPage
